097322eedad498ce1711dfa626f07fc4c80bf6a9
Atharva Jagtap <atharvacjagtap2005@gmail.com>
2026-01-08 22:29:36 +0530

    added

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "367964517"
+    serviceWorkerVersion: "2388909667"
   }
 });
